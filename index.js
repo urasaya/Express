8cd0586f25f22f20f2496b9a2cfcfcbcb3f9ffb4
urasaya <68938785+urasaya@users.noreply.github.com>
2021-08-20 19:12:57 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -21,14 +21,23 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended : true}))
 app.use(methodOverride('_method'))
 
+const categories = ['Fruit', 'Vegetable', 'Dairy']
+
 app.get('/products', async (req, res) =>{
-    const products = await Product.find({})
-    console.log(products)
-    res.render('products/index', { products })
+    const { category } = req.query
+    if(category){
+        const products = await Product.find({category})
+        res.render('products/index', { products, category })
+    } else{
+        const products = await Product.find({})
+        res.render('products/index', { products, category: 'All' })
+    }
+    
+    
 })
 
 app.get('/products/new', (req,res) =>{
-    res.render('products/new')
+    res.render('products/new', { categories })
 })
 
 app.post('/products', async (req,res) => {
@@ -47,13 +56,19 @@ app.get('/products/:id', async (req,res) => {
 app.get('/products/:id/edit',async (req,res) => {
     const {id} = req.params;
     const product = await Product.findById(id);
-    res.render('products/edit', { product })
+    res.render('products/edit', { product,categories })
 })
 
 app.put('/products/:id', async (req,res) => {
     const { id } = req.params
     const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true})
     res.redirect(`/products/${product._id}`)
+})
+
+app.delete('/products/:id', async (req,res) => {
+    const{ id } = req.params
+    const deletedProduct = await Product.findByIdAndDelete(id);
+    res.redirect('/products')
 })
 
 app.listen(3000, () =>{
